7dd14d9b94807c7c714f92952f015d878c309660
3m-code <mikolivepl@gmail.com>
2026-09-20 10:05:23 +0200

    Create ModInfo.cs

---
+++ src/ModInfo.cs
@@ -0,0 +1,7 @@
+namespace MoreUpgradedRewardsPreviews;
+
+public static class ModInfo
+{
+    public const string Id = "moreupgradedrewardspreviews";
+    public const string DisplayName = "More Upgraded Rewards Previews";
+}
