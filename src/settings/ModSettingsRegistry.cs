7b452f402d05505a87bb57345b625bd62bd6291b
3m-code <mikolivepl@gmail.com>
2026-09-20 20:11:10 +0200

    Update ModSettingsRegistry.cs

--- src/settings/ModSettingsRegistry.cs
+++ src/settings/ModSettingsRegistry.cs
@@ -9,6 +9,8 @@ public static class ModSettingsRegistry
         CombatPileSettingsConfig.Instance.Register();
         CardRewardSelectionScreenSettingsConfig.Instance.Register();
         CardRemovalSelectionScreenSettingsConfig.Instance.Register();
+        CardUpgradeSelectionScreenSettingsConfig.Instance.Register();
+        CardTransformSelectionScreenSettingsConfig.Instance.Register();
         CardDuplicateSelectionScreenSettingsConfig.Instance.Register();
         CardEnchantSelectionScreenSettingsConfig.Instance.Register();
         CardGeneratorSelectionScreenSettingsConfig.Instance.Register();
