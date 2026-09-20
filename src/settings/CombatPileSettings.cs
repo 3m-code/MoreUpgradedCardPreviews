f8a7ed751f709f45b12aad668d28c5f7d3bff92a
3m-code <mikolivepl@gmail.com>
2026-09-20 09:02:38 +0200

    Create CombatPileSettings.cs

---
+++ src/settings/CombatPileSettings.cs
@@ -0,0 +1,23 @@
+﻿using MegaCrit.Sts2.Core.Entities.Cards;
+
+namespace MoreUpgradedRewardsPreviews.Settings;
+
+public static class CombatPileSettings
+{
+    public static bool DrawPileEnabled { get; set; } = true;
+
+    public static bool DiscardPileEnabled { get; set; } = true;
+
+    public static bool ExhaustPileEnabled { get; set; } = true;
+
+    public static bool IsEnabled(PileType pileType)
+    {
+        return pileType switch
+        {
+            PileType.Draw => DrawPileEnabled,
+            PileType.Discard => DiscardPileEnabled,
+            PileType.Exhaust => ExhaustPileEnabled,
+            _ => false
+        };
+    }
+}
