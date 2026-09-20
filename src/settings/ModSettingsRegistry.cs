4c42f96a557ffbee4a41fd5666f7a9161d15d6eb
3m-code <mikolivepl@gmail.com>
2026-09-20 11:04:03 +0200

    Update ModSettingsRegistry.cs

--- src/settings/ModSettingsRegistry.cs
+++ src/settings/ModSettingsRegistry.cs
@@ -5,6 +5,7 @@ public static class ModSettingsRegistry
     public static void Register()
     {
         CardRewardSelectionScreenSettingsConfig.Register();
+        CardGeneratorSelectionScreenSettingsConfig.Register();
         CombatPileSettingsConfig.Register();
     }
 }
