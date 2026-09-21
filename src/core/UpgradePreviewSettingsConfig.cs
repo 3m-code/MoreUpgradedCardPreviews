b7cff43a7ac50f65566beae06ca04a87dff6c867
3m-code <mikolivepl@gmail.com>
2026-09-21 09:14:10 +0200

    Update UpgradePreviewSettingsConfig.cs

--- src/core/UpgradePreviewSettingsConfig.cs
+++ src/core/UpgradePreviewSettingsConfig.cs
@@ -8,6 +8,9 @@ namespace MoreUpgradedRewardsPreviews.Core;
 
 public abstract class SingleUpgradePreviewSettingsConfig<TSettings> where TSettings : class, new()
 {
+    private readonly string _dataKey;
+    private readonly string _fileName;
+
     private readonly Func<TSettings, bool> _getEnabled;
     private readonly Action<TSettings, bool> _setEnabled;
     private readonly Func<TSettings> _defaultFactory;
@@ -15,22 +18,14 @@ public abstract class SingleUpgradePreviewSettingsConfig<TSettings> where TSetti
     private readonly ModSettingsValueBinding<TSettings, bool> _upgradePreviewBinding;
 
     protected SingleUpgradePreviewSettingsConfig(
-        string pageId,
         string dataKey,
         string fileName,
-        string pageTitle,
-        string pageDescription,
-        string settingLabel,
         Func<TSettings, bool> getEnabled,
         Action<TSettings, bool> setEnabled,
         Func<TSettings> defaultFactory)
     {
-        PageId = pageId;
-        DataKey = dataKey;
-        FileName = fileName;
-        PageTitle = pageTitle;
-        PageDescription = pageDescription;
-        SettingLabel = settingLabel;
+        _dataKey = dataKey;
+        _fileName = fileName;
 
         _getEnabled = getEnabled;
         _setEnabled = setEnabled;
@@ -38,25 +33,16 @@ public abstract class SingleUpgradePreviewSettingsConfig<TSettings> where TSetti
 
         _upgradePreviewBinding = new(
             ModInfo.Id,
-            DataKey,
+            _dataKey,
             SaveScope.Global,
             _getEnabled,
-            SetUpgradePreviewEnabled);
+            SetUpgradePreviewEnabled
+            );
     }
 
-    private string PageId { get; }
-
-    private string DataKey { get; }
-
-    private string FileName { get; }
-
-    private string PageTitle { get; }
-
-    private string PageDescription { get; }
-
-    private string SettingLabel { get; }
-
     public event Action<bool>? UpgradePreviewEnabledChanged;
+
+    public ModSettingsValueBinding<TSettings, bool> UpgradePreviewBinding => _upgradePreviewBinding;
 
     public bool IsUpgradePreviewEnabled()
     {
@@ -76,10 +62,7 @@ public abstract class SingleUpgradePreviewSettingsConfig<TSettings> where TSetti
     public void Register()
     {
         RegisterData();
-        RegisterSettingsPage();
     }
-
-    protected ModSettingsValueBinding<TSettings, bool> UpgradePreviewBinding => _upgradePreviewBinding;
 
     private void RegisterData()
     {
@@ -88,28 +71,12 @@ public abstract class SingleUpgradePreviewSettingsConfig<TSettings> where TSetti
             var store = RitsuLibFramework.GetDataStore(ModInfo.Id);
 
             store.Register(
-                key: DataKey,
-                fileName: FileName,
+                key: _dataKey,
+                fileName: _fileName,
                 scope: SaveScope.Global,
                 defaultFactory: _defaultFactory,
                 autoCreateIfMissing: true);
         }
-    }
-
-    private void RegisterSettingsPage()
-    {
-        RitsuLibFramework.RegisterModSettings(
-            ModInfo.Id,
-            page =>
-            {
-                page.WithTitle(ModSettingsText.Literal(PageTitle))
-                    .WithModDisplayName(ModSettingsText.Literal(ModInfo.DisplayName))
-                    .WithDescription(ModSettingsText.Literal(PageDescription))
-                    .WithVisibleOnHostSurfaces(ModSettingsHostSurface.All)
-                    .AddSection("upgrade_preview", section => section.WithTitle(ModSettingsText.Literal("Upgrade Preview"))
-                            .AddToggle("enabled", ModSettingsText.Literal(SettingLabel), _upgradePreviewBinding));
-            },
-            PageId);
     }
 
     private void SetUpgradePreviewEnabled(TSettings settings, bool value)
